a03630042c88c9d3dfcebe2c62753ca4fcc1f898
bjkim <bjkim@mnwquit>
2024-12-04 21:00:44 +0900

    MyBatis Mapper 빈으로 등록

--- src/main/java/com/example/testboard/mapper/MemberMapper.java
+++ src/main/java/com/example/testboard/mapper/MemberMapper.java
@@ -1,7 +1,9 @@
 package com.example.testboard.mapper;
 
 import com.example.testboard.dto.MemberDto;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface MemberMapper {
 
     public void insertMember(MemberDto memberDto);
