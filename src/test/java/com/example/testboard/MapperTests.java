cb84bc861aee4b61f18384b2f937b590f301778f
bjkim <bjkim@mnwquit>
2024-12-04 21:02:54 +0900

    MapperTests 테스트 코드 작성

--- src/test/java/com/example/testboard/MapperTests.java
+++ src/test/java/com/example/testboard/MapperTests.java
@@ -1,4 +1,31 @@
 package com.example.testboard;
 
+import com.example.testboard.dto.MemberDto;
+import com.example.testboard.mapper.MemberMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class MapperTests {
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Test
+    public void testInsert() {
+
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName("장발장");
+        memberDto.setId("mr.bal");
+        memberDto.setPhone("010-111-2222");
+
+        System.out.println(memberDto);
+        memberMapper.insertMember(memberDto);
+
+        System.out.println("--------------------------------------");
+        System.out.println("레코드가 추가되었습니다.");
+        System.out.println("--------------------------------------");
+    }
+
 }
